023604fd09d641861a36124301899abe78fe344b
Vriddhi Gupta <146509993+vriddhig17@users.noreply.github.com>
2026-04-22 23:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (84.90%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // 1. Find the first decreasing element from the right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // 2. If such an element exists, find the smallest number 
+        // to its right that is larger than nums[i] and swap them
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // 3. Reverse the elements to the right of index i to get
+        // the smallest possible lexicographical order for that part
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+}
